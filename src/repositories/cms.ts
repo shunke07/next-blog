6d326ee08dc6271bc4760a32f2a61de1e447e858
shunke07 <s.takami07@gmail.com>
2021-01-14 21:18:23 +0900

    Add API for CMS

---
+++ src/repositories/cms.ts
@@ -0,0 +1,29 @@
+import { Article, Response } from '@/types/cms'
+
+const endpoint = process.env.CMS_ENDPOINT
+const config = {
+  headers: {
+    'X-API-KEY': process.env.CMS_API_KEY,
+  },
+}
+
+export const api = async <T>(url: string): Promise<T> => {
+  const response = await fetch(url, config)
+  if (!response.ok) {
+    throw new Error(response.statusText)
+  }
+  const json = await response.json()
+  return json
+}
+
+export const fetchArticle = async (articleId: string): Promise<Article> => {
+  const url = `${endpoint}/${articleId}`
+  const result = await api<Article>(url)
+  return result
+}
+
+export const fetchArticles = async (limit?: number): Promise<Article[]> => {
+  const url = limit ? `${endpoint}?limit=${limit}` : endpoint
+  const { contents } = await api<Response>(url)
+  return contents
+}
